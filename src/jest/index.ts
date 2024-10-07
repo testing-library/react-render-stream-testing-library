57636558b561fbc0d5829d538009b6fb873eb82d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-10-07 15:27:13 +0200

    more cleanup

--- src/jest/index.ts
+++ src/jest/index.ts
@@ -1,7 +1,34 @@
 import { expect } from "@jest/globals";
 import { toRerender, toRenderExactlyTimes } from "./ProfiledComponent.js";
+import type {
+  NextRenderOptions,
+  Profiler,
+  ProfiledComponent,
+  ProfiledHook,
+} from "../profile/index.js";
 
 expect.extend({
   toRerender,
   toRenderExactlyTimes,
 });
+interface ApolloCustomMatchers<R = void, T = {}> {
+  toRerender: T extends
+    | Profiler<any>
+    | ProfiledComponent<any, any>
+    | ProfiledHook<any, any>
+    ? (options?: NextRenderOptions) => Promise<R>
+    : { error: "matcher needs to be called on a ProfiledComponent instance" };
+
+  toRenderExactlyTimes: T extends
+    | Profiler<any>
+    | ProfiledComponent<any, any>
+    | ProfiledHook<any, any>
+    ? (count: number, options?: NextRenderOptions) => Promise<R>
+    : { error: "matcher needs to be called on a ProfiledComponent instance" };
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R = void, T = {}> extends ApolloCustomMatchers<R, T> {}
+  }
+}
