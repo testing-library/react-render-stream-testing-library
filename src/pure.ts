92cea83984f2e9be89066f204bc1eef7084e9c1d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-11-26 11:53:18 +0100

    add missing exports

--- src/pure.ts
+++ src/pure.ts
@@ -12,9 +12,15 @@ export {useTrackRenders} from './renderStream/useTrackRenders.js'
 
 export type {SyncScreen} from './renderStream/Render.js'
 
-export {renderToRenderStream} from './renderToRenderStream.js'
+export {
+  renderToRenderStream,
+  renderToAsyncRenderStream,
+} from './renderToRenderStream.js'
 export type {RenderStreamWithRenderResult} from './renderToRenderStream.js'
-export {renderHookToSnapshotStream} from './renderHookToSnapshotStream.js'
+export {
+  renderHookToSnapshotStream,
+  renderHookToAsyncSnapshotStream,
+} from './renderHookToSnapshotStream.js'
 export type {SnapshotStream} from './renderHookToSnapshotStream.js'
 
 export type {Assertable} from './assertable.js'
