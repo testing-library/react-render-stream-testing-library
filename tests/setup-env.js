932f93885e08b9eeafdd005b036ae44238a0d601
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-11-29 13:33:38 +0100

    guard in tests against accidental `IS_REACT_ACT_ENVIRONMENT`

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,1 +1,15 @@
 import './polyfill.js'
+
+Object.defineProperty(global, 'IS_REACT_ACT_ENVIRONMENT', {
+  get() {
+    return false
+  },
+  set(value) {
+    if (!!value) {
+      throw new Error(
+        'Cannot set IS_REACT_ACT_ENVIRONMENT to true, this probably pulled in some RTL dependency?',
+      )
+    }
+  },
+  configurable: true,
+})
